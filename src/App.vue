1d56a8c5af09de72f80884f28c88224e5dbd3bac
Ronald Almeida <ronaldalmeida20022003@gmail.com>
2023-12-07 16:50:12 -0300

    starting project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import Dialog from './components/dialog.vue';
+  import Button from './components/button.vue';
+</script>
+
+<template>
+  <Dialog>
+    <h1>TESTANDO</h1>
+    <Button content="TESTANDO"></Button>
+  </Dialog>
+</template>
+
+<style>
+body {
+  background-color: #111;
+}
+</style>
---
+++ src/components/button.vue
@@ -0,0 +1,22 @@
+<template>
+    <button>
+        <template v-if="!$slots.default">{{ content }}</template>
+        <slot></slot>
+    </button>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    type TActions = "close" | "accept"
+
+    export default defineComponent({
+        props:{
+            content: String,
+            action: {
+                type: String as () => TActions,
+                default: "close"
+            }
+        }
+    })
+</script>
