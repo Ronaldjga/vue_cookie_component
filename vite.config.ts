301d3dcc4183331041bcc222a33bbfb4f4049ca7
Ronald Almeida <ronald.almeida@prodepa.pa.gov.br>
2024-01-02 11:03:48 -0300

    update ts and vite config to export types in lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import typescript from "@rollup/plugin-typescript"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [{
+    ...typescript({tsconfig: "./tsconfig.json"}),
+    apply: "build",
+    declaration: true,
+    declarationDir: "types/",
+    rootDir: "/"
+  },vue()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
