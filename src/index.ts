4e1cae2d5db70f8838d839fab08f45e999c853af
Ronald Almeida <ronald.almeida@prodepa.pa.gov.br>
2024-01-02 13:53:46 -0300

    types new try

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import cookieDialog from "./components/cookie-dialog.vue";
 import cookieButton from "./components/cookie-button.vue";
 import cookieActions from "./components/cookie-actions.vue";
 
-export {
-    cookieDialog,
-    cookieButton,
-    cookieActions
-}
+export {cookieDialog}
+export {cookieButton}
+export {cookieActions}
