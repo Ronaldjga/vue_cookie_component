56f098bf38859da2845f1675d81bd196bdc91eb1
Ronald Almeida <ronald.almeida@prodepa.pa.gov.br>
2024-01-02 14:14:10 -0300

    new try

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,11 @@
-declare module "vue-cookie-component"
+import type cookieButton from "../src/components/cookie-button.vue"
+import type cookieDialog from "../src/components/cookie-dialog.vue"
+import type cookieActions from "../src/components/cookie-actions.vue"
+
+declare module "vue-cookie-component" {
+    interface GlobalComponents {
+        cookieButton: typeof cookieButton,
+        cookieDialog: typeof cookieDialog,
+        cookieActions: typeof cookieActions
+    }
+}
