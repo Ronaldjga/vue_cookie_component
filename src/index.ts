b634f4e3ececc6b9c1cefcd93c11d38c43ff51b5
Ronald Almeida <ronald.almeida@prodepa.pa.gov.br>
2024-01-02 15:09:21 -0300

    export components for desestructurinig in import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-import cookieDialog from "./components/cookie-dialog.vue";
-import cookieButton from "./components/cookie-button.vue";
-import cookieActions from "./components/cookie-actions.vue";
-
-export default {
-    cookieDialog,
-    cookieButton,
-    cookieActions
-}
+// src/index.ts
+export { default as cookieDialog } from "./components/cookie-dialog.vue";
+export { default as cookieButton } from "./components/cookie-button.vue";
+export { default as cookieActions } from "./components/cookie-actions.vue";
